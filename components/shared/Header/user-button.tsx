7c71d7b731ae34b77f5d7c69ebc622317426cedc
ookayE <smith.evan93@gmail.com>
2025-02-16 00:59:46 -0500

    user button added, signUp schema added to validators, signUp action created

--- components/shared/Header/user-button.tsx
+++ components/shared/Header/user-button.tsx
@@ -39,11 +39,28 @@ const UserButton = async () => {
             </Button>
           </div>
         </DropdownMenuTrigger>
-        <DropdownMenuContent
-          className="w-56"
-          align="end"
-          forceMount
-        ></DropdownMenuContent>
+        <DropdownMenuContent className="w-56" align="end" forceMount>
+          <DropdownMenuLabel className="font-normal">
+            <div className="flex flex-col space-y-1">
+              <div className="text-sm font-medium leading-none">
+                {session.user?.name}
+              </div>
+              <div className="text-sm text-muted-foreground leading-none">
+                {session.user?.email}
+              </div>
+            </div>
+          </DropdownMenuLabel>
+          <DropdownMenuItem className="p-0 mb-1">
+            <form action={signOutUser} className="w-full">
+              <Button
+                className="w-full py-4 px-2 h-4 justify-start"
+                variant="ghost"
+              >
+                Sign Out
+              </Button>
+            </form>
+          </DropdownMenuItem>
+        </DropdownMenuContent>
       </DropdownMenu>
     </div>
   );
